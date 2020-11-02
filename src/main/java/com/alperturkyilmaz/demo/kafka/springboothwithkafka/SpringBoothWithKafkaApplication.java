f5f55fe95236253a24ca05e25d102913291ce0e7
Turkyilmaz, Alper <alper.turkyilmaz@devoteam.com>
2020-11-02 15:04:40 +0100

    first commit

---
+++ src/main/java/com/alperturkyilmaz/demo/kafka/springboothwithkafka/SpringBoothWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.alperturkyilmaz.demo.kafka.springboothwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoothWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoothWithKafkaApplication.class, args);
+	}
+
+}
